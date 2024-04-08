6aff04c2d6e148f9d79adff7cd007a0edf677bc8
IIIAMANE <147691401+IIIAMANE@users.noreply.github.com>
2024-04-08 21:54:53 +0500

    Add files via upload

---
+++ bvbv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Проверяем, установлен ли Python
+if ! command -v python3 &> /dev/null; then
+    echo "Python не установлен. Установка Python..."
+    sudo apt update
+    sudo apt install python3 -y
+fi
+
+# Скачиваем ваш Python-скрипт
+echo "Скачиваем ваш Python-скрипт..."
+curl -O <https://raw.githubusercontent.com/IIIAMANE/script/main/uwu.py>
+
+# Запускаем Python-скрипт
+echo "Запускаем ваш Python-скрипт..."
+python3 uwu.py
